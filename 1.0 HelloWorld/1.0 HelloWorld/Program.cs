5911d8c0c124dc7e39e57be126e2e20f900d3d2f
aterakrofi <aterakrofi@gmail.com>
2018-06-12 07:53:06 -0400

    //Name of class . Method of class (invoke , call or do it now with this )             //; end of instruction             //Whitespaces do not matter in c#

---
+++ 1.0 HelloWorld/1.0 HelloWorld/Program.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1._0_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Name of class . Method of class (invoke , call or do it now with this )
+            //; end of instruction
+            //Whitespaces do not matter in c#
+            
+            Console.WriteLine("Hello World");
+            Console.ReadLine();     
+        }
+    }
+}
