81b7c072338407737dd1568e6db3509f80d0f68c
aterakrofi <aterakrofi@gmail.com>
2018-06-18 07:29:54 -0400

    Variables

---
+++ Variables/Variables/Program.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;   // -2.14billion to +2.14billion size
+            int y;
+
+            x = 7;
+            y = x + 3;
+
+            Console.WriteLine(y);
+            Console.ReadLine();
+
+        }
+    }
+}
